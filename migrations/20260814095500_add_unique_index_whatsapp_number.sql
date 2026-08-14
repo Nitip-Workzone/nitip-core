d7099896c443ab2ce10340ac786f7f8aadefbb4d
IPKIN9 <irwandirini@gmail.com>
2026-08-14 11:25:37 +0800

    Update goose

--- migrations/20260814095500_add_unique_index_whatsapp_number.sql
+++ migrations/20260814095500_add_unique_index_whatsapp_number.sql
@@ -1,5 +1,9 @@
--- Up
+-- +goose Up
+-- +goose StatementBegin
 CREATE UNIQUE INDEX idx_users_unique_whatsapp ON users(whatsapp_number) WHERE deleted_at IS NULL;
+-- +goose StatementEnd
 
--- Down
+-- +goose Down
+-- +goose StatementBegin
 DROP INDEX IF EXISTS idx_users_unique_whatsapp;
+-- +goose StatementEnd
